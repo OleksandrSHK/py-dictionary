57bcc317da81398022ce8d31dae58f1c47770ad8
Oleksandr Shkliar <shklyara29@gmail.com>
2025-09-26 17:35:21 +0300

    Corrected solution

--- app/main.py
+++ app/main.py
@@ -3,97 +3,94 @@ from typing import Any
 
 class Dictionary:
     def __init__(self) -> None:
-        self.my_dict = []
+        self.capacity = 8  # initial capacity
+        self.hash_table = [None] * self.capacity  # pre-selected list
+        self.length = 0  # numbers of elements
+
+    def resize(self) -> None:
+        old_table = self.hash_table
+        self.capacity *= 2
+        self.hash_table = [None] * self.capacity
+        self.length = 0
+        for node in old_table:
+            if node is not None:
+                self.__setitem__(node[0], node[2])
 
     def __setitem__(self, key: Any, value: Any) -> None:
-        for member in self.my_dict:
-            if member[0] == key:
-                member[1] = value
+        if self.length >= int(self.capacity * 2 / 3):
+            self.resize()
+
+        key_hash = hash(key)
+        index = key_hash % self.capacity
+        node = [key, key_hash, value]
+
+        while self.hash_table[index] is not None:
+            if self.hash_table[index][0] == key:
+                self.hash_table[index][2] = value
                 return
-        self.my_dict += [[key, value]]
+            index = (index + 1) % self.capacity
+        self.hash_table[index] = node
+        self.length += 1
 
     def __getitem__(self, key: Any) -> Any:
-        for member in self.my_dict:
-            if member[0] == key:
-                return member[1]
+        key_hash = hash(key)
+        index = key_hash % self.capacity
+        while self.hash_table[index] is not None:
+            if self.hash_table[index][0] == key:
+                return self.hash_table[index][2]
+            index = (index + 1) % self.capacity
         raise KeyError(f"Key {key} is not found")
 
     def __len__(self) -> int:
-        amount = 0
-        for member in self.my_dict:
-            if member:
-                amount += 1
-        return amount
+        return self.length
 
-    def __clear__(self) -> None:
-        keys = []
-        for k in self.my_dict:
-            keys += [k]
-        for key in keys:
-            del self.my_dict[key]
+    def clear(self) -> None:
+        self.capacity = 8
+        self.hash_table = [None] * self.capacity
+        self.length = 0
 
-    def __delitem__(self, key: Any) -> None | str:
-        for k in self.my_dict:
-            if k == key:
-                del self.my_dict[key]
-            else:
-                return "Error: key not found"
+    def __delitem__(self, key: Any) -> None:
+        key_hash = hash(key)
+        index = key_hash % self.capacity
+        while self.hash_table[index] is not None:
+            if self.hash_table[index][0] == key:
+                self.hash_table[index] = None
+                self.length -= 1
+                return
+            index = (index + 1) % self.capacity
+        raise KeyError(f"Key {key} is not found")
 
-    def get(self, key: Any) -> bool:
-        for k in self.my_dict:
-            if k == key:
-                return True
-        return False
+    def get(self, key: Any) -> Any | None:
+        key_hash = hash(key)
+        index = key_hash % self.capacity
+        while self.hash_table[index] is not None:
+            if self.hash_table[index][0] == key:
+                return self.hash_table[index][2]
+            index = (index + 1) % self.capacity
+        return None
 
     def pop(self, key: Any) -> Any:
-        result = 0
-        goal = False
-        for k in self.my_dict:
-            if k == key:
-                result = self.my_dict[k]
-                goal = True
-                break
-        if not goal:
-            return "Error: key not found"
-        del self.my_dict[key]
-        return result
+        key_hash = hash(key)
+        index = key_hash % self.capacity
+        while self.hash_table[index] is not None:
+            if self.hash_table[index][0] == key:
+                result = self.hash_table[index][2]
+                self.hash_table[index] = None
+                self.length -= 1
+                return result
+            index = (index + 1) % self.capacity
+        raise KeyError(f"Key {key} is not found")
 
     def update(self, key: Any, value: Any) -> None:
-        amount = 0
-        count = 0
-        for k in self.my_dict:
-            if k == key:
-                self.my_dict[k] = value
-                break
-            if k != key:
-                amount += 1
-                count += 1
-        if count == amount:
-            self.my_dict[key] = value
+        self.__setitem__(key, value)
 
     def __iter__(self) -> Any:
+        self.iter_index = 0
         return self
 
-
-class Node:
-    def __init__(self, key: Any, value: Any) -> None:
-        self.key = key
-        self.hash = hash(key)
-        self.value = value
-
-
-class HashTable:
-    def __init__(self) -> None:
-        self.hash_table = []
-
-    def get_insert(self, key: Any, value: Any) -> None:
-        node = Node(key, value)
-        self.hash_table.append(node)
-
-    def get_find(self, key: Any) -> Any:
-        key_hash = hash(key)
-        for node in self.hash_table:
-            if node.hash == key_hash and node.key == key:
-                return node.value
-            else:
-                return None
+    def __next__(self) -> None:
+        while self.iter_index < self.capacity:
+            for node in self.hash_table:
+                if node is not None:
+                    yield node[0]
+        raise StopIteration
